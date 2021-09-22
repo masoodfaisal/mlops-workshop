c127cd180d425aba046d2c117ffb4374659b35b6
Ross Brigoli <rbrigoli@redhat.com>
2021-09-22 19:39:12 +0800

    Fix using predict_raw() to override JSON deserialization

--- seldon/drift_deploy/CustomerChurnDriftPredictor.py
+++ seldon/drift_deploy/CustomerChurnDriftPredictor.py
@@ -1,11 +1,31 @@
 import joblib
-
+import pandas as pd
+import numpy as np
+from alibi_detect.utils.saving import load_detector
+import json
 
 class CustomerChurnDriftPredictor(object):
 
     def __init__(self):
-        self.model = joblib.load('CustomerChurnDriftPredictor.sav')
+        self.model = joblib.load('CustomerChurnDriftModel.sav')
+        #self.model = load_detector('model')
 
-    def predict(self, X, features_names):
+    def predict_raw(self, request):
+        data = request.get("data", {}).get("ndarray")
+        mult_types_array = np.array(data, dtype=object)
 
-        return self.model.predict(X)
+        result = self.model.predict(mult_types_array)
+        # print(result)
+        # print(type(result))
+        return json.dumps(result, cls=JsonSerializer)
+
+class JsonSerializer(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (
+        np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
