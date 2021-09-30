87f39fb01340292c18125f7cd6d3d81332dbb2e9
Ross <brigoli@gmail.com>
2021-09-30 10:50:43 +0000

    Wait for spark cluster to be ready before proceeding :beers:

--- airflow/spark/start-spark-cluster.py
+++ airflow/spark/start-spark-cluster.py
@@ -1,38 +1,77 @@
 import openshift as oc
 import os
+import time
 from jinja2 import Template
 
 print("Start OCP things...")
 
+# Get the Openshift API URL
 server = "https://" + os.environ["KUBERNETES_SERVICE_HOST"] + ":" + os.environ["KUBERNETES_SERVICE_PORT"] 
+
 print(server)
 
-#build from source Docker file
+print("Retrieving Openshift token information...")
+# Get the Openshift auth token
 with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as file:
     token = file.read()
-print(f"Openshift Token{token}")
+# print(f"Openshift Token{token}")
 
-#/var/run/secrets/kubernetes.io/serviceaccount
+print("Retrieving Openshift project information...")
+# Get the current Openshift project name
 with open('/var/run/secrets/kubernetes.io/serviceaccount/namespace', 'r') as namespace:
     project = namespace.read()
 print(f"Project name: {project}")
 
+spark_crd_name = "SparkCluster"
+cluster_prefix = "spark-cluster"
 cluster_name = os.environ["SPARK_CLUSTER"]
+timeout_seconds = 120
 
-#build from source Docker file
+
+# run predicate func every 3 seconds until it returns true
+def wait(predicate, timeout):
+    mustend = time.time() + timeout
+    while time.time() < mustend:
+        if predicate: return True
+        time.sleep(3)
+    return False
+
+
+# Connect to Openshift
 with oc.api_server(server):
     with oc.token(token):
         with oc.project(project), oc.timeout(10*60):
             print('OpenShift client version: {}'.format(oc.get_client_version()))
-            
-            template_data = {"clustername": cluster_name, "project": "anz-ml"}
-            applied_template = Template(open("spark-cluster.yaml").read())
-            print(applied_template.render(template_data))
-            oc.create(applied_template.render(template_data))
-            
+
+            print(f"Searching for SparkCluster with name {cluster_prefix}-{cluster_name}...")
+            cluster_count = oc.selector(f"{spark_crd_name}/{cluster_prefix}-{cluster_name}").count_existing()
+            print(f"SparkCluster found: {cluster_count}")
+
+            # Only create Spark cluster if it doesn't exist
+            if cluster_count == 0:
+
+                template_data = {"clustername": f"{cluster_prefix}-{cluster_name}"}
+                applied_template = Template(open("spark-cluster.yaml").read())
+
+                print(f"Creating SparkCluster {cluster_prefix}-{cluster_name} ...")
+                # print(applied_template.render(template_data))
+                oc.create(applied_template.render(template_data))
+
+                ready_pred = oc.selector(f"{spark_crd_name}/{cluster_prefix}-{cluster_name}")\
+                    .object().model.status.conditions.can_match({'status': "ready"})
+
+                # wait for cluster to be ready
+                ready = wait(ready_pred, timeout_seconds)
+
+                if not ready:
+                    print(f"Cluster was not ready after a given time {timeout_seconds}s")
+
+                print(f"SparkCluster {cluster_prefix}-{cluster_name} created")
+                # task_instance = context['task_instance']
+                # Pulls the return_value XCOM from "pushing_task"
+                # task_instance.xcom_push(key="spark_cluster", value="ross")
+            else:
+                print(f"Spark Cluster already exists {cluster_prefix}-{cluster_name}")
+
             with open('spark-info.txt', 'w') as file:
-                file.write(f"spark-cluster-{cluster_name}")
-                
-#             task_instance = context['task_instance']
-            # Pulls the return_value XCOM from "pushing_task"
-            #task_instance.xcom_push(key="spark_cluster", value="ross")
+                    file.write(f"{cluster_prefix}-{cluster_name}")
