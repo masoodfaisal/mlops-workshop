80c3049660fc4dffe0c24c5c23026f5daf67cbb1
Ross <brigoli@gmail.com>
2021-10-05 08:10:12 +0000

    Pushed missing files

---
+++ airflow/spark/run-pyspark-job.py
@@ -0,0 +1,17 @@
+import os
+from pyspark.sql import SparkSession
+import spark_util
+
+def init_spark():
+    spark = spark_util.getOrCreateSparkSession("Pyspark Hello World")
+    sc = spark.sparkContext
+    return spark, sc
+
+def main():
+    spark,sc = init_spark()
+    nums = sc.parallelize([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+    print(nums.map(lambda x: x*x).collect())
+
+
+if __name__ == '__main__':
+    main()
